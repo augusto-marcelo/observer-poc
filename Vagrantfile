5fb3c84e2e2e776d2184131c8d2005db75abd045
marcelo <augustoribeiro.marcal@gmail.com>
2022-04-30 16:20:53 +0100

    Improved shell script for provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -2,13 +2,13 @@ Vagrant.configure("2") do |config|
   config.vm.box = "debian/buster64"
   
   
-  config.vm.define "alura-observer" do |pgr1|
-    pgr1.vm.hostname = "observer"
+  config.vm.define "alura-observer-5" do |pgr1|
+    pgr1.vm.hostname = "observer05"
     pgr1.vm.network "private_network", ip: "192.168.56.20" #bridge: 'wlp3s0' 
     pgr1.vm.provision "shell", path: "initial_setup.sh"
 
     pgr1.vm.provider "virtualbox" do |p1|
-        p1.name = "alura-observer"
+        p1.name = "alura-observer-5"
         p1.gui = false
         p1.memory = 2048
         p1.cpus = 2
--- initial_setup.sh
+++ initial_setup.sh
@@ -1,37 +1,93 @@
 #!/bin/bash
 
-apt update -y
+DOCKER_INSTALL_SCRIPT_URL="https://get.docker.com"
+DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)"
+APACHE_MAVEN_TAR_URL="https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz"
 
-apt install -y git curl wget
+REQUIRED_TOOLS="git curl wget"
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-
-sh get-docker.sh
-
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-chmod +x /usr/local/bin/docker-compose
-
-## Java jdk 1.8 from https://jdk.java.net/java-se-ri/8-MR3
-#https://download.java.net/openjdk/jdk8u41/ri/openjdk-8u41-b04-linux-x64-14_jan_2020.tar.gz
-
-# Maven from https://maven.apache.org/download.cgi
-# https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
+# Directory structure
+DOWNLOAD_DIR="/tmp/provisioning_files"
+JAVA_DIR="/opt/java/"
+MVN_DIR="/opt/maven"
 
 
-####
-## Need to add this to the /root/.bashrc file
-####
-#export JAVA_HOME="/opt/java-se-8u41-ri"
-## Maven
-#export M2_HOME="/opt/apache-maven-3.8.5"
-#export M2="$M2_HOME/bin"
-#export PATH="$PATH:$JAVA_HOME/bin"
-#export PATH="$PATH:$M2"
+function stage_zero() {
+    echo "#### Starting stage 0: ... ####"
+    
+    #update pkg manager
+    apt update -y
+    apt install -y $REQUIRED_TOOLS
+
+    # create base directory structure
+    mkdir -p $DOWNLOAD_DIR
+    mkdir -p $JAVA_DIR
+    mkdir -p $MVN_DIR
+
+    cp -r /vagrant/tmp/observer_realm /root/
+    chown -R root:root /root/observer_realm
+
+    # Configure bashrc
+    cp /vagrant/tmp/observer_realm/bashrc /root/.bashrc
+
+    # Configure timezone
+    timedatectl set-timezone Europe/Lisbon
+
+    echo "#### Stage 0 completed succesfully ###"
+}
+
+function stage_one() {
+    echo "#### Starting stage 1: ... ####"
+
+    cd $DOWNLOAD_DIR
+    curl -fsSL $DOCKER_INSTALL_SCRIPT_URL -o get-docker.sh
+    curl -L $DOCKER_COMPOSE_URL -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+    wget $APACHE_MAVEN_TAR_URL
+
+    echo "#### Stage 1 completed succesfully ###"
+}
+
+function stage_two() {
+    echo "#### Starting stage 2: .... ####"
+
+    cd $DOWNLOAD_DIR
+    
+    sh get-docker.sh
+    
+    tar xzvf apache-maven-3.8.5-bin.tar.gz
+    mv apache-maven-3.8.5 $MVN_DIR/apache-maven-3.8.5
+
+    cp /vagrant/tmp/observer_realm/jdk-8u202-linux-x64.tar.gz $DOWNLOAD_DIR
+    tar xzvf jdk-8u202-linux-x64.tar.gz
+    mv jdk1.8.0_202 $JAVA_DIR
+    
+
+    echo "#### Stage 2 completed succesfully ###"
+}
+
+function stage_three() {
+    echo "#### Starting stage 3: .... ####"
+
+    cd /root/observer_realm
+    docker-compose up -d
+
+    echo "#### Stage 3 completed succesfully ###"
+}
+
+function main() {
+
+    stage_zero
+    stage_one
+    stage_two
+    stage_three
+
+}
+
+main
 
 ### TO-DO
 ## Fix date and time by:
-#       1 - Adjust the timezone on timedatectl
+#       1 - Adjust the timezone on timedatectl [DONE]
 #       2 - Enable ntp service 
 #       3 - Configure timezone servers
 #       4 - Make sure ntp service is running
